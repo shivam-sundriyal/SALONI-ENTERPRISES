2a988f965875b3fed375a119621bf6e1a95e39bd
Shivam Sundriyal <shivamsundriyal22@gmail.com>
2024-01-28 22:37:15 +0530

    Completed Website

--- main.js
+++ main.js
@@ -15,22 +15,148 @@ if (searchClose) {
   });
 }
 
+/* ============== SHADOW HEADER ============== */
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+this.scrollY >= 50 ? header.classList.add('shadow-header')
+                    :header.classList.remove('shadow-header');
+}
+
+window.addEventListener('scroll', shadowHeader);
+
 /* ============== HOME SWIPER ============== */
 let swiperHome = new Swiper(".home__swiper", {
   loop: true,
-  spaceBetween: -5,
+  spaceBetween: -24,
   grabCursor: true,
   slidesPerView: 'auto',
   centeredSlides: 'auto',
 
   autoplay: {
-    delay: 2000,
+    delay: 3000,
     disableOnInteraction: false,
   },
 
   breakpoints: {
     1220: {
-        spaceBetween: -5,
+        spaceBetween: -32,
     }
   }
 });
+
+/* ============== FEATURED SWIPER ============== */
+let swiperFeatured = new Swiper(".featured__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 4,
+      centeredSlides: false
+    },
+  },
+});
+
+/* ============== TESTIMONIAL SWIPER ============== */
+let swiperTestimonial = new Swiper(".testimonial__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+      centeredSlides: false, 
+    },
+  },
+});
+
+/* ============== TESTIMONIAL SWIPER ============== */
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp)
+
+/* ============== SCROLL SECTIONS ACTIVE LINK ============== */
+const sections = document.querySelectorAll('section[id')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link');
+        } else {
+            sectionClass.classList.remove('active-link');
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
+
+/* ============== DARK LIGHT THEME ============== */
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/* ============== SCROLL REVEAL ANIMATION ============== */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__data, .featured__container, .testimonial__container, .footer`);
+sr.reveal(`.home__images`, {delay: 600})
+sr.reveal(`.services__card`, {interval: 100})
